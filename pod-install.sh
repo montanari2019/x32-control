a70159931b60a60d0d7942c3d358c15f025ec9fa
Ikaro Montanari <ikaromontanari@MacBook-Air-de-Taciane.local>
2026-05-13 10:09:36 -0400

    ajustando ites do ios para compilacao final

--- pod-install.sh
+++ pod-install.sh
@@ -6,6 +6,7 @@ echo "$unameOut"
 
 if [ "$unameOut" = "Darwin" ] && [ -z "${CI}" ]; then
     cd ios || exit 1
+    export BUNDLE_PATH="vendor/bundle"
     bundle install
     bundle exec pod install
 fi
